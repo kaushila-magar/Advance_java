77bd9c41fe0f859bc0d9d0eab4ab35bd6d8a16dd
kaushila-magar <kaushilathapamagar8@gmail.com>
2026-09-12 22:07:52 +0545

    Frame

---
+++ MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class MyFrame{
+public static void main(String[]args){
+    JFrame frame =
+    new JFrame ("My Calculator");
+    frame.setSize(800,400);
+    frame.setDefaultCloseOperation(
+        JFrame.EXIT_ON_CLOSE
+    );
+    frame.setVisible(true);
+
+}
+}
